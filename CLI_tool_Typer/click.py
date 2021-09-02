5d784e7388e5d0817f6bdd820f8d620b42673a65
Shilpa106 <shilpadevi.bluethink@gmail.com>
2021-09-02 19:27:53 +0530

    first commit

---
+++ CLI_tool_Typer/click.py
@@ -0,0 +1,26 @@
+# import click
+
+# @click.group()
+# def main():
+#     pass
+
+# @main.command()
+# @click.argument("x", type=float)
+# @click.argument("y", type=float)
+# def add (x, y):
+#     print("%s + %s = %s" % (x, y, x+y))
+
+
+# @main.command()
+# @click.argument("x", type=int)
+# @click.argument("y", type=int)
+# def sub (x, y):
+#     print("%s - %s = %s" % (x, y, x-y))
+
+
+# if __name__=='__main__':
+#     main()
+
+
+
+
